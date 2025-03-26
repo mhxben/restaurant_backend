805dff341ff9be449342a68e2814c0e59c10a00b
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2025-03-26 17:28:06 +0100

    fix database engine fallback for local dev

--- restaurant_backend/settings.py
+++ restaurant_backend/settings.py
@@ -81,9 +81,18 @@ WSGI_APPLICATION = 'restaurant_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')),
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
